648aea781ee9f7fade8980c6c24318e35f363795
Yu-Chun <dyj0183@byui.edu>
2024-08-25 22:28:06 -0600

    added second chain to have a sequencial chain

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
+
 from dotenv import load_dotenv
+
 import argparse
 
 load_dotenv()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--task", default="Return a list of numbers")
+parser.add_argument("--task", default="loops through 1-10")
 parser.add_argument("--language", default="python")
 args = parser.parse_args()
 
@@ -19,6 +21,11 @@ code_prompt = PromptTemplate(
   input_variables=["language", "task"]
 )
 
+second_prompt = PromptTemplate(
+  input_variables=["language", "code"],
+  template="write a test for the following {language} code:\n {code}"
+)
+
 code_chain = LLMChain(
   llm=llm,
   prompt=code_prompt,
@@ -26,7 +33,19 @@ code_chain = LLMChain(
   output_key="code"
 )
 
-result = code_chain({
+second_chain = LLMChain(
+  llm=llm,
+  prompt=second_prompt,
+  output_key="test",
+)
+
+chain = SequentialChain(
+  chains=[code_chain, second_chain],
+  input_variables=["task", "language"],
+  output_variables=["test", "code"],
+)
+
+result = chain({
   "language": args.language,
   "task": args.task
 })
